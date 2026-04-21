1e19c8d6f4bee0a41926a3f4fd5d1731bbb47741
Yuk1no <121278340+yukno1@users.noreply.github.com>
2026-04-21 22:14:32 +0800

    2026.4.21

---
+++ src/lexer.rs
@@ -0,0 +1,62 @@
+#[derive(Debug)]
+pub struct Lexer<'a> {
+    content: &'a [char],
+}
+
+impl<'a> Lexer<'a> {
+    pub fn new(content: &'a [char]) -> Self {
+        Self { content }
+    }
+
+    fn trim_left(&mut self) {
+        // trim whitespace from left
+        while self.content.len() > 0 && self.content[0].is_whitespace() {
+            self.content = &self.content[1..];
+        }
+    }
+
+    fn chop(&mut self, n: usize) -> &'a [char] {
+        let token = &self.content[0..n];
+        self.content = &self.content[n..];
+        token
+    }
+
+    // same way in take_while
+    fn chop_while<P>(&mut self, mut predicate: P) -> &'a [char]
+    where
+        P: FnMut(&char) -> bool,
+    {
+        let mut n = 0;
+        while n < self.content.len() && predicate(&self.content[n]) {
+            n += 1;
+        }
+        self.chop(n)
+    }
+
+    fn next_token(&mut self) -> Option<&'a [char]> {
+        self.trim_left();
+        if self.content.len() == 0 {
+            return None;
+        }
+
+        if self.content[0].is_numeric() {
+            return Some(self.chop_while(|x| x.is_numeric()));
+        }
+
+        if self.content[0].is_alphabetic() || self.content[0] == '_' {
+            return Some(self.chop_while(|x| x.is_alphanumeric() || *x == '_'));
+        }
+
+        // if weird char, treat as single token
+        Some(self.chop(1))
+        // todo!("Invalid token start with {start}", start = self.content[0])
+    }
+}
+
+impl<'a> Iterator for Lexer<'a> {
+    type Item = &'a [char];
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next_token()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,82 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    collections::HashMap,
+    fs::{self, File},
+    io,
+    path::{Path, PathBuf},
+};
+use xml::reader::{EventReader, XmlEvent};
+
+use crate::lexer::Lexer;
+
+mod lexer;
+fn index_document(doc_content: &str) -> HashMap<String, usize> {
+    todo!()
+}
+
+fn read_entire_xml_file<P: AsRef<Path>>(file_path: P) -> io::Result<String> {
+    let file = File::open(file_path)?;
+    let er = EventReader::new(file);
+
+    let mut content = String::new();
+    for event in er {
+        if let XmlEvent::Characters(text) = event.expect("TODO") {
+            content.push_str(&text);
+            content.push_str(" ");
+        }
+    }
+    Ok(content)
+}
+
+type TermFreq = HashMap<String, usize>;
+type TermFreqIndex = HashMap<PathBuf, TermFreq>;
+
+fn main() -> io::Result<()> {
+    let dir_path = "docs.gl\\gl4";
+    let dir = fs::read_dir(dir_path)?;
+    let top_n = 10;
+    let mut tf_index = TermFreqIndex::new();
+
+    for file in dir {
+        let file_path = file?.path();
+
+        println!("Indexing {:?}...", &file_path);
+
+        let content = read_entire_xml_file(&file_path)?
+            .chars()
+            .collect::<Vec<_>>();
+
+        // term frequency table
+        let mut tf = TermFreq::new();
+
+        let lexer = Lexer::new(&content);
+
+        for token in lexer {
+            let term = token
+                .iter()
+                .map(|x| x.to_ascii_uppercase())
+                .collect::<String>();
+
+            if let Some(freq) = tf.get_mut(&term) {
+                *freq += 1;
+            } else {
+                tf.insert(term, 1);
+            }
+        }
+
+        // let mut stats = tf.iter().collect::<Vec<_>>();
+        // stats.sort_by_key(|(_, f)| *f);
+        // stats.reverse();
+
+        tf_index.insert(file_path, tf);
+        // println!("{file_path:?}");
+        // for (t, f) in stats.iter().take(top_n) {
+        //     println!("    {t} => {f}");
+        // }
+    }
+
+    for (path, tf) in tf_index {
+        println!("{path:?} has {count} unique tokens", count = tf.len());
+    }
+
+    Ok(())
 }
